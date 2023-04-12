5691eb182910a1b0324314eb0fde83ecefef1fe7
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-04-12 06:06:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-	return 'Hello World'
+	return 'Hello World awab'
 
 # main driver function
 if __name__ == '__main__':
